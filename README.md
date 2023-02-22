31c7f8f5deb592630a5ca4e6d7e24e239d405ea8
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-02-22 12:04:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-for-java
+ Docker for java Developer 
---
+++ src/main/java/com/example/dockerforjava/DockerForJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerforjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerForJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerForJavaApplication.class, args);
+	}
+
+}
